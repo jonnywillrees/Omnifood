4ab9c71b7c1edf07572d266cc3dbd6657b5755e7
jonnywillrees <jonnywillrees@gmail.com>
2020-12-10 11:45:48 +0200

    fix a small bug relating redirect link after email is sent

--- mailer-new.php
+++ mailer-new.php
@@ -8,7 +8,7 @@
 
     // Check the data.
     if (empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        header("Location: https://jonnywillrees.github.io/Omnifood?success=-1#form");
+        header("Location: http://jonnytesthostingpackage-com.stackstaging.com/omnifood/index.php/?success=-1#form");
         exit;
     }
 
@@ -30,6 +30,6 @@
     mail($recipient, $subject, $email_content, $email_headers);
     
     // Redirect to the index.html page with success code
-    header("Location: https://jonnywillrees.github.io/Omnifood?success=1#form");
+    header("Location: http://jonnytesthostingpackage-com.stackstaging.com/omnifood/index.php?success=1#form");
 
 ?>
